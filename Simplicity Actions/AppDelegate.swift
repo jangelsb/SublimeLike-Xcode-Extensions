a4e7448f4cbf9c63ee6fa930195e86342723425a
jangelsb <jangelsb@uci.edu>
2019-07-20 09:54:39 -0700

    Initial Commit

---
+++ Simplicity Actions/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Simplicity Actions
+//
+//  Created by Josh Angelsberg on 7/20/19.
+//  Copyright © 2019 Josh Angelsberg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Simplicity Actions/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Simplicity Actions
+//
+//  Created by Josh Angelsberg on 7/20/19.
+//  Copyright © 2019 Josh Angelsberg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
